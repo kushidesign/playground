b268d37759e047515daacfbb12f394ba3a7c67ec
Jeremiah Coyle <jcoyle@gmail.com>
2022-12-12 07:16:15 -0800

    Remove dep

--- project.clj
+++ project.clj
@@ -1,11 +1,10 @@
-(defproject design.kushi/playground "0.1.1"
+(defproject design.kushi/playground "0.1.2"
   :description "Playground is an interactive documentation system for ClojureScript UI projects."
   :url "github.com/kushidesign/playground"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[reagent/reagent                       "1.1.0"]
                  [applied-science/js-interop            "0.3.3"]
-                 [org.clojars.paintparty/kushi          "1.0.0-a.15"]
                  [binaryage/devtools                    "1.0.3"]
                  [metosin/malli                         "0.8.4"]
                  [markdown-to-hiccup/markdown-to-hiccup "0.6.2"]]
