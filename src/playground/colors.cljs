77f80e9e7525d616e81077c5024fff94f93feee6
Jeremiah Coyle <jcoyle@gmail.com>
2022-12-11 20:52:33 -0800

    color scales layout

--- src/playground/colors.cljs
+++ src/playground/colors.cljs
@@ -10,6 +10,7 @@
    [kushi.ui.core :refer [defcom]]
    [kushi.ui.icon.mui.core :refer [mui-icon]]
    [kushi.color :refer [base-color-map]]
+   [kushi.colors :as kushi.colors]
    [kushi.ui.label.core :refer [label]]))
 
 (defcom text-sample-sticker
@@ -19,9 +20,9 @@
                        :.small
                        :ai--c
                        :w--44px
-                       :md:w--54px
+                       :sm:w--54px
                        :h--44px
-                       :md:h--54px
+                       :sm:h--54px
                        [:c color]
                        [:bgc bgc]
                        :border-radius--50%
@@ -37,17 +38,23 @@
    {:on-click          #(copy-to-clipboard s)
     :-tooltip-placement :inline-end}]])
 
-(defn color-modal [{:keys [k hsl color-name]}]
+(defn color-modal
+  [{:keys [k
+           hsl
+           color-name
+           color-level]
+    :as m}]
   [modal
    (sx :.flex-col-c
        {:-trigger     [label (sx :.pointer
                                  :.code
                                  :&.code:bgc--transparent
                                  :fs--0.7em
-                                 :md:fs--0.9em
+                                 :sm:fs--0.9em
                                  :ws--n
                                  {:on-click open-kushi-modal})
-                       k
+                       [:span (sx :sm:d--none) color-level]
+                       [:span (sx :sm:d--block :d--none) k]
                        [mui-icon (sx :mis--0.5em) :help-outline]]
         :-scrim-attrs (sx ["dark:bgc" '(rgba 30 30 30 0.86)])
         :-panel-attrs (sx :h--600px
@@ -85,28 +92,43 @@
        [:span.meta-desc-label "rgb"] [copy-color rgb]])]] )
 
 
-
 (defn color-rows [color-scales]
   [:<>
-   (let [row-height    :75px
-         circle-height :54px]
-     (for [{:keys [color-name scale]} color-scales]
+   (let [row-height   :75px
+         kushi-colors (reduce (fn [acc [k v]]
+                                (assoc acc (keyword k) v))
+                              {}
+                              (partition 2 kushi.colors/colors))]
+     (for [{:keys [color-name scale]} color-scales
+           :let [semantic-alias (some-> kushi-colors
+                                        (get color-name)
+                                        :alias)]]
        (into
         ^{:key color-name}
         [:div (sx 'color-scale-wrapper
                   :.transition
                   :dark:bgc--black
                   :dark:outline--1rem:solid:black
-                  :mbs--115px)
-         [:h3 (sx :.xlarge :.wee-bold :tt--capitalize :mbe--2.5em) color-name]]
-        (for [[k v] scale
+                  :mbs--85px)
+         [:h3 (sx :.xlarge
+                  :.wee-bold
+                  :tt--capitalize)
+          color-name]
+         [:p (sx :.normal
+                 :mb--1em:2.5em)
+          "All "
+          [:code (str "--" color-name)]
+          " values on the scale have a corresponding "
+          [:code (str "--" semantic-alias)]
+          " alias token"]]
+        (for [[k v color-level] scale
               :let  [hsl (if (number? v) (str v) (name v))]]
           ^{:key hsl}
           [:div (sx :.flex-row-fs
                     [:h row-height])
-           [:div (sx :md:flex-basis--150px
-                     :width--44px
-                     :md:width--unset
+           [:div (sx :sm:flex-basis--150px
+                     :width--66px
+                     :sm:width--unset
                      :flex-grow--0
                      :flex-shrink--0
                      [:bgc hsl])]
@@ -116,7 +138,7 @@
                      :bbes--solid
                      :bbew--1px
                      [:bbec hsl])
-            [color-modal (keyed k hsl color-name)]
+            [color-modal (keyed k hsl color-name color-level)]
             [:div (sx :.flex-row-fe)
              [text-sample-sticker (sx {:-color :white
                                        :-bgc   hsl})]
